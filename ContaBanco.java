e65eff2f4f8ef78015feec26bb2c05fb75d9b718
Leonardo <108599618+LeoXavier95@users.noreply.github.com>
2023-04-16 01:09:23 -0300

    Projeto sem o main finalizado

---
+++ ContaBanco.java
@@ -0,0 +1,112 @@
+import javax.swing.JOptionPane;
+
+public class ContaBanco {
+    public int numConta;
+    protected char tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+
+    public void estadoAtual(){
+        JOptionPane.showInputDialog(null,"Digite se é fisica ou corrente");
+    }
+
+    public ContaBanco(){
+      this.setSaldo(0);; 
+      this.setStatus(false);;
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public void abrirConta(char tipo){
+        this.setTipo(tipo);
+        this.setStatus(true);
+        if (tipo == 'c' || tipo == 'C') {
+            this.setSaldo(50.00f);
+        } else if(tipo == 'p' || tipo == 'P') {
+            this.setSaldo(150.00f);;
+        }
+    }
+
+    public void fecharConta(){
+        if (this.saldo > 0) {
+            JOptionPane.showMessageDialog(null, "Retire todo o dinheiro da sua conta");
+         }else if(this.saldo < 0) {
+            JOptionPane.showMessageDialog(null, "Conta em débito");
+         }else {
+            JOptionPane.showMessageDialog(null, "Sua Conta foi fechada com sucesso ");
+        }
+
+    }
+
+    public void depositar(float valor){
+        if (this.status == true) {
+            setSaldo(getSaldo() + valor);
+        }else{
+            JOptionPane.showMessageDialog(null, "Impossivel depositar");
+        }
+    }
+
+    public void sacar(float valor){
+        if (this.getStatus()) {
+        if(this.getSaldo() >= valor){
+            this.setSaldo(this.getSaldo() - valor);
+        }else{
+            JOptionPane.showMessageDialog(null, "Impossivel sacar dinheiro");
+        }
+    }
+    }
+
+    public void  pagarMensal(){
+         float valor = 0;
+        if (this.getTipo() == 'c' || this.getTipo() == 'C') {
+           valor = 15.00f;
+        } else {
+            valor = 20.00f;
+        }
+        if(this.status == true){
+            setSaldo(getSaldo() - valor);
+        }else{
+            JOptionPane.showMessageDialog(null, "Saldo Insuficiente");
+        }
+    }
+
+}
